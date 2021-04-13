21196da426d6008fcc1d97d1f3ebead9b9357670
ryougishiki1999 <171860634@smail.nju.edu.cn>
2021-04-13 23:18:13 +0800

    优化PoseEstimationSolver,并且添加对选定点深度的测量

--- app/src/main/java/com/nju/cs/zrh/objecttracking/PoseEstimationSolver.java
+++ app/src/main/java/com/nju/cs/zrh/objecttracking/PoseEstimationSolver.java
@@ -8,15 +8,23 @@ import com.nju.cs.zrh.objecttracking.utils.poseestimation.OnPoseEstimation;
 import com.nju.cs.zrh.objecttracking.utils.poseestimation.PoseEstimation2D2D;
 
 import org.opencv.core.CvType;
+import org.opencv.core.KeyPoint;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfDMatch;
 import org.opencv.core.MatOfKeyPoint;
+import org.opencv.core.Point;
 import org.opencv.core.Point3;
+import org.opencv.utils.Converters;
 
+import java.security.Key;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Map;
 
 public class PoseEstimationSolver {
 
@@ -25,6 +33,7 @@ public class PoseEstimationSolver {
     private LinkedList<Mat> rgbaFrameList = new LinkedList<>();
     private FeatureMatch mFeatureMatch = new ORBFeatureMatch();
     private OnPoseEstimation mOnPoseEstimation;
+    private Map<Mat, Point> touchMap = new HashMap<>();
 
     private Mat intrinsic = new Mat(3, 3, CvType.CV_64FC1);
 
@@ -34,11 +43,13 @@ public class PoseEstimationSolver {
     }
 
     public void processFrame(Mat rgbaFrame) {
+
         mFeatureMatch.featureDetectAndDraw(rgbaFrame);
     }
 
-    public void addRgbaFrame(Mat rgbaFrame) {
-        rgbaFrameList.add(rgbaFrame.clone());
+    public void addRgbaFrame(Mat rgbaFrame, Point point) {
+        rgbaFrameList.add(rgbaFrame);
+        touchMap.put(rgbaFrame, point);
 
         if (rgbaFrameList.size() >= 2) {
             poseEstimation();
@@ -46,9 +57,10 @@ public class PoseEstimationSolver {
     }
 
     private void poseEstimation() {
-        ListIterator<Mat> iterator = rgbaFrameList.listIterator(rgbaFrameList.size() - 1);
-        Mat img1 = iterator.previous().clone();
-        Mat img2 = iterator.next().clone();
+        ListIterator<Mat> iterator1 = rgbaFrameList.listIterator(rgbaFrameList.size() - 1);
+        ListIterator<Mat> iterator2 = rgbaFrameList.listIterator(rgbaFrameList.size() - 1);
+        Mat img1 = iterator1.previous();
+        Mat img2 = iterator2.next();
 
         MatOfKeyPoint keyPoint1 = new MatOfKeyPoint();
         MatOfKeyPoint keyPoint2 = new MatOfKeyPoint();
@@ -59,8 +71,19 @@ public class PoseEstimationSolver {
         Mat T = new Mat(3, 1, CvType.CV_64FC1);
         mOnPoseEstimation.estimation(keyPoint1, keyPoint2, match, R, T);
 
-        Log.i(TAG, "Rotation Matrix:" + R.dump());
-        Log.i(TAG, "Translation Vector" + T.dump());
+        //debug
+        Log.d(TAG, "Rotation Matrix:" + R.dump());
+        Log.d(TAG, "Translation Vector" + T.dump());
+
+        Point p1 = touchMap.get(img1);
+        Point p2 = touchMap.get(img2);
+        List<KeyPoint> keyPointList1 = new ArrayList<>(keyPoint1.toList());
+        keyPointList1.add(new KeyPoint((float) p1.x, (float) p1.y, 1.0f));
+        List<KeyPoint> keyPointList2 = new ArrayList<>(keyPoint2.toList());
+        keyPointList2.add(new KeyPoint((float) p2.x, (float) p2.y, 1.0f));
+
+        keyPoint1.fromList(keyPointList1);
+        keyPoint2.fromList(keyPointList2);
 
         List<Point3> world_coordinate_pts = new ArrayList<>();
         Triangulation.getInstance().triangulation(keyPoint1, keyPoint2, match, intrinsic, R, T, world_coordinate_pts);
